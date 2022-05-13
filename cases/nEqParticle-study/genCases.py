fd9b9a6c6c41fe8e608be0417b9e54838ea9d7bb
andytorrestb <andytorrestb@gmail.com>
2022-05-13 12:16:57 -0400

    nEqParticle-study: configure genCases.py to change nEquivalentParticles and maxRadialWeightFactor

--- cases/nEqParticle-study/genCases.py
+++ cases/nEqParticle-study/genCases.py
@@ -1,57 +1,43 @@
 import casefoam
 
-baseCase = '2d-wedge'
-
-caseStructure = [['nEquivalentParticles_01', 'nEquivalentParticles_02', 'nEquivalentParticles_03'],
-                ['vertical', 'diagonol', 'horizontal']]
-
+# Functions for updated variables in generated cases.
 def update_nEquivalentParticles(nEqPar):
     nEqPar = 'nEquivalentParticles            ' + nEqPar
     return {
-        'system/controlDict': {'#!stringManipulation':
+        'constant/dsmcProperties': {'#!stringManipulation':
                                 {'nEquivalentParticles            2e16': '%s' %nEqPar}}
     }
 
-def update_sampleDict(direction):
-    if (direction == 'horizontal'):
-        print(direction)
-        start = 'start ( 1.05 0.0 0.0 );'
-        end = 'end ( 20.0 0.0 0.0 );'
-        return {
-            'system/sampleDict': {'#!stringManipulation':
-                                    {'start ( 0.0 1.05 0.0 );': '%s' %start,
-                                     'end ( 0.0 20.0 0.0 );': '%s' %end}}
-        }
-    elif (direction == 'diagonol'):
-        print(direction)
-        start = 'start ( 0.7425 0.7425 0.0 );'
-        end = 'end ( 14.1421 14.1421 0.0 );'
-        return {
-            'system/sampleDict': {'#!stringManipulation':
-                                    {'start ( 0.0 1.05 0.0 );': '%s' %start,
-                                     'end ( 0.0 20.0 0.0 );': '%s' %end}}
-        }
-    elif (direction == 'vertical'):
-        print(direction)
-        start = 'start ( 0.0 1.05 0.0 );'
-        end = 'end ( 0.0 20.0 0.0 );'
-        return {
-            'system/sampleDict': {'#!stringManipulation':
-                                    {'start ( 0.0 1.05 0.0 );': '%s' %start,
-                                     'end ( 0.0 20.0 0.0 );': '%s' %end}}
-        }
-    else:
-        print("No Valid input")
-        return
+def update_maxRadialWeightingFactor(maxRadWeight):
+    maxRadWeight = 'maxRadialWeightingFactor            ' + maxRadWeight
+    return {
+        'constant/dsmcProperties': {'#!stringManipulation':
+                                {'maxRadialWeightingFactor    20': '%s' %maxRadWeight}}
+    }
+
+# Define case structure
+baseCase = '2d-wedge'
+nEqPars = ['nEquivalentParticles_01', 'nEquivalentParticles_02',
+                  'nEquivalentParticles_03', 'nEquivalentParticles_04',
+                  'nEquivalentParticles_05', 'nEquivalentParticles_06']
+
+maxRadWeights = ['maxRadialWeightingFactor_01', 'maxRadialWeightingFactor_02',
+                 'maxRadialWeightingFactor_03', 'maxRadialWeightingFactor_04']
+
+caseStructure = [nEqPars, maxRadWeights]
 
 caseData = {
     'nEquivalentParticles_01': update_nEquivalentParticles('2e16'),
     'nEquivalentParticles_02': update_nEquivalentParticles('1e16'),
     'nEquivalentParticles_03': update_nEquivalentParticles('5e15'),
-    'vertical':  update_sampleDict('vertical'),
-    'diagonol':  update_sampleDict('diagonol'),
-    'horizontal':  update_sampleDict('horizontal')
+    'nEquivalentParticles_04': update_nEquivalentParticles('2.5e15'),
+    'nEquivalentParticles_05': update_nEquivalentParticles('1.2e15'),
+    'nEquivalentParticles_06': update_nEquivalentParticles('6e14'),
+    'maxRadialWeightingFactor_01':  update_maxRadialWeightingFactor('125'),
+    'maxRadialWeightingFactor_02':  update_maxRadialWeightingFactor('250'),
+    'maxRadialWeightingFactor_03':  update_maxRadialWeightingFactor('500'),
+    'maxRadialWeightingFactor_04':  update_maxRadialWeightingFactor('1000')
 }
 
-# generate cases+
+# Generate cases
 casefoam.mkCases(baseCase, caseStructure, caseData, hierarchy='tree',writeDir='Cases')
