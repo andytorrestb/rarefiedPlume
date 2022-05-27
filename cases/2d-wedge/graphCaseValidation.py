55c9df2234279b2e8b927f61cc123fdd509e9aea
andytorrestb <andytorrestb@gmail.com>
2022-05-27 08:28:50 -0400

    2d-wedge: update validation graphs to solve several issues

--- cases/2d-wedge/graphCaseValidation.py
+++ cases/2d-wedge/graphCaseValidation.py
@@ -26,41 +26,72 @@ digi_n = pd.read_csv(
     )
 
 digi_T = pd.read_csv(
-    curr_dir_path + '/T_Tstar_radius.dat',
+    curr_dir_path + '/T_Tstar_radius_DAC.dat',
     header = 0,
     sep = '\t',
     names = ['r', 'T_Tstar']
     )
 
+# Read in simulated data. 
 sim = pd.read_csv(
     curr_dir_path + '/postProcessing/sampleDict/0.3/horizontalLine_Ttra_Ar_rhoN_Ar.csv'
     )
 
+# Used to see what the values trend to. 
 print(sim['Ttra_Ar'])
 
 sim = sim[['distance', 'rhoN_Ar', 'Ttra_Ar']].dropna()
-sim['rhoN_Ar'] = sim['rhoN_Ar'] / 6.02e20
-sim['Ttra_Ar'] = sim['Ttra_Ar'] / 800.0
+sim['rhoN_Ar'] = sim['rhoN_Ar'] / 8.377e20
+sim['Ttra_Ar'] = sim['Ttra_Ar'] / 1000.0
     
 
-plt.title('DSMC vs DAC', fontdict = font_axis_publish)
-plt.ylabel('n/n*', fontdict = font_axis_publish)
-plt.xlabel('r', fontdict = font_axis_publish)
+# Producde Analytical Data
+def TTt_Ma(Ma, ga = 1.4):
+    return (ga + 1) / (2 + (ga - 1) * Ma ** 2)
 
-plt.plot(digi_n['r'], digi_n['n_nstar'], label = 'digitized (DAC)')
-plt.plot(sim['distance'], sim['rhoN_Ar'], label = 'simulated (DSMC)')
+def rrt_Ma(Ma, ga = 1.4):
+  rrt = (1 / TTt_Ma(Ma, ga)) ** ((ga + 1) / (ga - 1))
+  rrt = np.sqrt(np.sqrt(rrt) / Ma)
+  return rrt
+
+def nnt_Ma(Ma, ga = 1.4):
+  return TTt_Ma(Ma, ga) ** (1 / (ga - 1))
+
+def a(T, ga = 1.4, R = 287):
+    return np.sqrt(ga * R * T)
+
+Ma_domain = np.linspace(1, 25, 100) 
+ga = 1.67
+TTt = TTt_Ma(Ma_domain, ga = ga)
+rrt = rrt_Ma(Ma_domain, ga = ga)
+nnt = nnt_Ma(Ma_domain, ga = ga)
+
+print("Printing rrt")
+print(rrt)
+
+# Graph Results
+plt.title('OpenFOAM vs DAC', fontdict = font_axis_publish)
+plt.ylabel('n/n*', fontdict = font_axis_publish)
+plt.xlabel('Radial distance, r (m)', fontdict = font_axis_publish)
+
+plt.plot(sim['distance'], sim['rhoN_Ar'], label = 'OpenFOAM (Torres, Pitt, Kinzel)')
+plt.plot(digi_n['r'], digi_n['n_nstar'], label = 'DAC (Lumpkin, Stewart)')
+plt.plot(rrt, nnt, label = 'Analytical Solution')
 plt.legend()
 plt.yscale('log')
+plt.ylim(bottom = 1e-4, top = 1)
 plt.savefig(curr_dir_path + '/digitized_vs_analytical_n.png')
 plt.close()
 
-plt.title('DSMC vs DAC', fontdict = font_axis_publish)
+plt.title('OpenFOAM vs DAC', fontdict = font_axis_publish)
 plt.ylabel('T/T*', fontdict = font_axis_publish)
-plt.xlabel('r', fontdict = font_axis_publish)
+plt.xlabel('Radial distance, r (m)', fontdict = font_axis_publish)
 
-plt.plot(digi_T['r'], digi_T['T_Tstar'], label = 'digitized (DAC)')
-plt.plot(sim['distance'], sim['Ttra_Ar'], label = 'simulated (DSMC)')
+plt.plot(sim['distance'], sim['Ttra_Ar'], label = 'OpenFOAM (Torres, Pitt, Kinzel)')
+plt.plot(digi_T['r'], digi_T['T_Tstar'], label = 'DAC (Lumpkin, Stewart)')
+plt.plot(rrt, TTt, label = 'Analytical Solution')
 plt.legend()
 plt.yscale('log')
+plt.ylim(bottom = 1e-3, top = 1)
 plt.savefig(curr_dir_path + '/digitized_vs_analytical_T.png')
 plt.close()
