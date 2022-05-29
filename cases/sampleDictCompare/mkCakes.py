c3e6e962e3d42f87cb187a1664ec8b5d465d7e2d
andytorrestb <andytorrestb@gmail.com>
2022-05-28 20:07:38 -0400

    sampleDictCompare: pseudo code for mkCases.py

---
+++ cases/sampleDictCompare/mkCakes.py
@@ -0,0 +1,14 @@
+# Pseudo code
+
+# Inputs
+# 1. Single variable to vary. 
+        # a. ID for variable
+        # b.  list of values to iterate over.
+# 2. template files for baseCase
+
+
+# Algorithm
+# 1. For each value in list,
+        # a. Make labeled directory to hold cases.
+        # b. Copy contents from baseCase to directory to labeled directory
+        # c. Replace values in variable of interest
